9cd24311512d51299815338ef55324c92bbd49b6
WillHK <will.haeck@gmail.com>
2015-04-03 16:16:50 -0700

    add dom manipulation, still buggy

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-(function () {
   var questions = ["Who is the voice actor for Homer?", "What is the name of the only bully with a child?", "Who is the news reporter on Channel 6 news?"];
   var answers = ["dan castellaneta", "kearney", "kent brockman"];
+  var simpsonsTrivia;
   var Game = function (questions, answers) {
     this.questions = questions;
     this.answers = answers;
@@ -8,6 +8,7 @@
     this.numberQuestions = questions.length;
     this.guess = '';
     this.questionNumber = 0;
+    console.log("Constructor used.");
   };
   Game.prototype.randomQuestion = function () {
     return Math.floor(Math.random() * this.questions.length);
@@ -19,7 +20,7 @@
     if (this.guess.toLowerCase() === this.answers[this.questionNumber]) {
       this.numberCorrect++;
       alert("Correct! " + (this.numberQuestions - this.numberCorrect) + " left");
-      this.removeCorrect(this.questionNumber);
+      this.removeCorrect();
     } else {
       alert("Sorry, that's not right. " + (this.numberQuestions - this.numberCorrect) + " left");
     }
@@ -28,21 +29,18 @@
     this.questions.splice(this.questionNumber, 1);
     this.answers.splice(this.questionNumber, 1);
   };
-  var simpsonsTrivia = new Game(questions, answers);
-  var onClick = function () {
+  simpsonsTrivia = new Game(questions, answers);
+  simpsonsTrivia.questionNumber = simpsonsTrivia.randomQuestion();
+  simpsonsTrivia.askQuestion();
+  if (simpsonsTrivia.numberCorrect === 3) {
+    document.getElementById('homer').innerHTML = "<img src = 'http://static.fjcdn.com/comments/Home+has+hidden+depths+unfortunately+they+are+filled+with+donuts+_7cb6c1d35d38ec82f99058af4b42f3b5.jpg'/>";
+  }
+var onSubmit = function () {
     simpsonsTrivia.guess = document.getElementById('answer').value;
     simpsonsTrivia.checkAnswer();
   };
-  simpsonsTrivia.questionNumber = simpsonsTrivia.randomQuestion();
-  simpsonsTrivia.askQuestion();
-  //console.log(simpsonsTrivia);
-  // while (simpsonsTrivia.numberCorrect < 3) {
-  //   alert("Let's play The Simpsons trivia! Answer all Question to Win!!!");
-  //     simpsonsTrivia.questionNumber = simpsonsTrivia.randomQuestion();
-  //     simpsonsTrivia.askQuestion();
-  // }
-  //alert("Congratulations! You Win!");
-  if (simpsonsTrivia.numberCorrect === 3) {
-    document.getElementById('homer').innerHTML = "<img src = 'http://static.fjcdn.com/comments/Home+has+hidden+depths+unfortunately+they+are+filled+with+donuts+_7cb6c1d35d38ec82f99058af4b42f3b5.jpg'></img>";
-  }
-})();
+var form = document.querySelector("form");
+  form.addEventListener("submit", function(event) {
+    console.log("Saving value", form.elements.value.value);
+    event.preventDefault();
+  });
