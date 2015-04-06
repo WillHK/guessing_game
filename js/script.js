cb8c6c410c4aee4a243914a36b7fe76fb1c00fb9
WillHK <will.haeck@gmail.com>
2015-04-05 17:38:02 -0700

    refactor into multiple choice quiz game with 20 questions total

--- js/script.js
+++ js/script.js
@@ -1,46 +1,159 @@
-  var questions = ["Who is the voice actor for Homer?", "What is the name of the only bully with a child?", "Who is the news reporter on Channel 6 news?"];
-  var answers = ["dan castellaneta", "kearney", "kent brockman"];
-  var simpsonsTrivia;
-  var Game = function (questions, answers) {
+
+var questions = [
+  {
+    "question": "What's Principal Skinner's REAL name?",
+    "answers": ["Robert Underdunk Terwilliger", "Armin Tamzarian", "Lyle Lanley"],
+    "correct": 1
+  },
+  {
+    "question": "Who voices Homer's half brother, Herbert Powell?",
+    "answers": ["Danny DeVito", "Phil Hartman", "Jon Lovitz"],
+    "correct": 0
+  },
+  {
+    "question": "What is Sideshow Bob's son's name?",
+    "answers": ["Gianni", "Gino", "Giovanni"],
+    "correct": 1
+  },
+  {
+    "question": "What is Homer Simpson's middle name?",
+    "answers": ["Jerry", "JoJo", "Jay"],
+    "correct": 2
+  },
+  {
+    "question": "Why does Homer end up in a mental institution, where he meets \"Michael Jackson\"?",
+    "answers": ["He wears a pink shirt to work", "He does a poor job as Mr. Burns' assistant", "He choked the boy ONE too many times"],
+    "correct": 0
+  },
+  {
+    "question": "What's the name of the energy bar that Homer endorses?",
+    "answers": ["Lightspeed Energy Bar", "Buzz Energy Bar", "PowerSauce"],
+    "correct": 2
+  },
+  {
+    "question": "What common garden item is Sideshow Bob's arch nemesis?",
+    "answers": ["Hoses", "Rakes", "Brooms"],
+    "correct": 1
+  },
+  {
+    "question": "Who are the guest stars in the \"Lisa the Vegetarian\" episode?",
+    "answers": ["Paul and Linda McCartney", "Alec Baldwin and Kim Basinger", "Gillian Anderson and David Duchovny"],
+    "correct": 0
+  },
+  {
+    "question": "Who has had a creepy unrequited crush on Marge since high school?",
+    "answers": ["Moe Syzslak", "Artie Ziff", "Seymour Skinner"],
+    "correct": 1
+  },
+  {
+    "question": "What is Chief Wiggum's first name?",
+    "answers": ["Clancy", "Carlton", "Clarent"],
+    "correct": 0
+  },
+  {
+    "question": "What is the name of Radioactive Man's sidekick?",
+    "answers": ["Toxic Avenger", "Radioactive Boy", "Fallout Boy"],
+    "correct": 2
+  },
+  {
+    "question": "What is the secret ingredient in a Flaming Moe?",
+    "answers": ["Vodka", "Blueberry Syrup", "Cough Medicine"],
+    "correct": 2
+  },
+  {
+    "question": "What is Marge's maiden name?",
+    "answers": ["Onassis", "Bouvier", "Kennedy"],
+    "correct": 1
+  },
+  {
+    "question": "What is the name of the company owned by Homer's former boss Hank Scorpio",
+    "answers": ["Globex", "Scorpio Industries", "Compu-Global-Hyper-Net"],
+    "correct": 0
+  },
+  {
+    "question": "What game company released a Simpson's game around the same time as The Simpsons Movie?",
+    "answers": ["Bethesda Softworks", "Blizzard-Activision", "Electronic Arts"],
+    "correct": 2
+  },
+  {
+    "question": "What name does Homer have his legally changed to when a TV show steals his?",
+    "answers": ["Max Power", "Guy Incognito", "Ned Flanders"],
+    "correct": 0
+  },
+  {
+    "question": "What drink is Homer disgusted by in the episode \"Homer vs New York\"?",
+    "answers": ["Clam Juice", "Goat's milk", "Mountain Dew"],
+    "correct": 2
+  },
+  {
+    "question": "What was the original color of Bart's shirt?",
+    "answers": ["Blue", "Orange", "Red"],
+    "correct": 0
+  },
+  {
+    "question": "They have ________ on computers now? - Homer Simpson",
+    "answers": ["Games", "The Internet", "TV"],
+    "correct": 1
+  },
+  {
+    "question": "Which friend does homer have to beat out so he can go on to be the first \"Average Joe\" in space?",
+    "answers": ["Moe", "Barney", "Lenny"],
+    "correct": 1
+  }
+];
+var Game = function (questions) {
     this.questions = questions;
-    this.answers = answers;
     this.numberCorrect = 0;
     this.numberQuestions = questions.length;
     this.guess = '';
+    this.answer = 0;
     this.questionNumber = 0;
-    console.log("Constructor used.");
   };
-  Game.prototype.randomQuestion = function () {
-    return Math.floor(Math.random() * this.questions.length);
-  };
-  Game.prototype.askQuestion = function () {
-    document.getElementById('question').innerHTML = this.questions[this.questionNumber];
-  };
-  Game.prototype.checkAnswer = function () {
-    if (this.guess.toLowerCase() === this.answers[this.questionNumber]) {
-      this.numberCorrect++;
-      alert("Correct! " + (this.numberQuestions - this.numberCorrect) + " left");
-      this.removeCorrect();
+Game.prototype.randomQuestion = function () {
+  return Math.floor(Math.random() * this.questions.length);
+};
+Game.prototype.askQuestion = function () {
+  this.questionNumber = this.randomQuestion();
+  var currentQuestionObject = this.questions[this.questionNumber];
+  var currentQuestion = currentQuestionObject["question"];
+  var currentAnswers = currentQuestionObject["answers"];
+  this.answer = currentQuestionObject["correct"];
+  document.getElementById('question').textContent = currentQuestion;
+  document.getElementById('answer0').textContent = currentAnswers[0];
+  document.getElementById('answer1').textContent = currentAnswers[1];
+  document.getElementById('answer2').textContent = currentAnswers[2];
+};
+Game.prototype.checkAnswer = function (answer) {
+  if (answer === this.answer) {
+    document.getElementById('score').textContent = ++this.numberCorrect;
+    if (this.numberCorrect === 20) {
+      this.victory();
     } else {
-      alert("Sorry, that's not right. " + (this.numberQuestions - this.numberCorrect) + " left");
+      this.removeCorrect();
     }
-  };
-  Game.prototype.removeCorrect = function () {
-    this.questions.splice(this.questionNumber, 1);
-    this.answers.splice(this.questionNumber, 1);
-  };
-  simpsonsTrivia = new Game(questions, answers);
-  simpsonsTrivia.questionNumber = simpsonsTrivia.randomQuestion();
-  simpsonsTrivia.askQuestion();
-  if (simpsonsTrivia.numberCorrect === 3) {
-    document.getElementById('homer').innerHTML = "<img src = 'http://static.fjcdn.com/comments/Home+has+hidden+depths+unfortunately+they+are+filled+with+donuts+_7cb6c1d35d38ec82f99058af4b42f3b5.jpg'/>";
   }
-var onSubmit = function () {
-    simpsonsTrivia.guess = document.getElementById('answer').value;
-    simpsonsTrivia.checkAnswer();
-  };
-var form = document.querySelector("form");
-  form.addEventListener("submit", function(event) {
-    console.log("Saving value", form.elements.value.value);
-    event.preventDefault();
-  });
+};
+Game.prototype.removeCorrect = function () {
+  this.questions.splice(this.questionNumber, 1);
+  this.askQuestion();
+};
+Game.prototype.victory = function () {
+  document.getElementById('main').textContent = "YOU WIN!!!!";
+};
+var simpsonsTrivia = new Game(questions);
+simpsonsTrivia.askQuestion();
+var button0 = document.getElementById('answer0');
+var button1 = document.getElementById('answer1');
+var button2 = document.getElementById('answer2');
+button0.addEventListener("click", function (event) {
+  event.preventDefault();
+  simpsonsTrivia.checkAnswer(0);
+});
+button1.addEventListener("click", function (event) {
+  event.preventDefault();
+  simpsonsTrivia.checkAnswer(1);
+});
+button2.addEventListener("click", function (event) {
+  event.preventDefault();
+  simpsonsTrivia.checkAnswer(2);
+});
